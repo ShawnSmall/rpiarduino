b3aeb635bfe4174706493a48645c46047fff3765
ShawnSmall <smsmall@umass.edu>
2020-10-09 22:43:05 +0100

    Serial Connection part 2

--- ard-episode-5-potentiometer/ard-episode-5-potentiometer.ino
+++ ard-episode-5-potentiometer/ard-episode-5-potentiometer.ino
@@ -9,14 +9,15 @@ void loop() {
   int pot;
   digitalWrite(led, HIGH);
   pot = analogRead(A5);
-  // this prints a line with the potentiometer reading
-  Serial.println(pot);
+  // The Serial.print command doesn't issue a new line
+  Serial.print(pot);
   delay(pot);
+  // so we can print a comma
+  Serial.print(",");
+  // then make a new reading
+  pot = analogRead(A5);
+  // And print another value with a newline
+  Serial.println(pot);
   digitalWrite(led, LOW);
   delay(pot);
 }
-
-
-
-
-
