35fb3be6eaf8f2ea0bd2ab4d99b5a6d75f1bc072
ShawnSmall <smsmall@umass.edu>
2020-10-09 15:46:50 +0100

    Revised blink-two.ino

--- blink-two/blink-two.ino
+++ blink-two/blink-two.ino
@@ -10,42 +10,42 @@ void loop() {
 }
 
 
-int led=6;
-void setup() {
-   pinMode(led, OUTPUT);
-}
 
-void loop() {
-  //gradually increase time pin is HIGH
-  for(int i=0; i<255; i++){
-  analogWrite(led, i);
-  delay(5);
-  }
-  //gradually decrease time pin is HIGH
-  for(int i=255; i>0; i--){
-  analogWrite(led, i);
-  delay(5);
-  }
-}
 
-int led=13;
-// attach a wire to pin 3
-int wire=3;
-void setup() {
-  pinMode(led, OUTPUT);
-  // configure the pin for INPUT
-  pinMode(wire, INPUT);
-  }
 
-void loop() {
-  // the pin reads HIGH, blink the LED.
-  if (digitalRead(wire)) {
-     digitalWrite(led, HIGH);
-     delay(500);
-     ditigalWrite(led, LOW);
-     delay(500);
-     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
