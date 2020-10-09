40839729b4b877f1141241960cb3362d93df956c
ShawnSmall <smsmall@umass.edu>
2020-10-09 20:42:57 +0100

    Potentiometer serial connection

--- ard-episode-5-potentiometer/ard-episode-5-potentiometer.ino
+++ ard-episode-5-potentiometer/ard-episode-5-potentiometer.ino
@@ -1,12 +1,16 @@
 int led = 13;
 void setup() {
  pinMode(led, OUTPUT);
+ // This turns on the serial connection
+ Serial.begin(9600);
 }
 
 void loop() {
   int pot;
   digitalWrite(led, HIGH);
   pot = analogRead(A5);
+  // this prints a line with the potentiometer reading
+  Serial.println(pot);
   delay(pot);
   digitalWrite(led, LOW);
   delay(pot);
