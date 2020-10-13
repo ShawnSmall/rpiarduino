01eb4aae60d0a1316afc054fcfb76b0d1b90d085
ShawnSmall <smsmall@umass.edu>
2020-10-13 16:17:02 +0100

    corrected typo

--- ard-episode-7-TimedBlink/ard-episode-7-TimedBlink.ino
+++ ard-episode-7-TimedBlink/ard-episode-7-TimedBlink.ino
@@ -2,17 +2,17 @@
 #include <TimedBlink.h>
 int led=6;
 
-TimedBlink monitor(led)
+TimedBlink monitor(led);
 
 void setup() {
- pinMode(led, OUTPUT);
- // Configure the function with times to stay on and off
- monitor.blink(500,500);
+  pinMode(led, OUTPUT);
+  // Configure the function with times to stay on and off
+  monitor.blink(500,500);
 }
 
 void loop() {
- // call frequently to blink at scheduled times.
- monitor.blink();
+  // call frequently to blink at scheduled times.
+  monitor.blink();
 }
 
 
