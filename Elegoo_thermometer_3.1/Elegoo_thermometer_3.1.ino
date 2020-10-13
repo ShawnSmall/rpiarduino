35b354034130e140b9e5d3f3b7bea2f613953ec2
ShawnSmall <smsmall@umass.edu>
2020-10-13 22:44:19 +0100

    thermistor code only

--- Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
+++ Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
@@ -1,19 +1,7 @@
-//www.elegoo.com
-//2016.12.9
-
-#include <LiquidCrystal.h>
-int tempPin = 0;
-//                BS  E  D4 D5  D6 D7
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup()
-{
-  /*lcd.begin(cols, rows)
-    lcd: a variable of type LiquidCrystal
-    cols: the number of columns that the display has
-    rows: the number of rows that the display has 
-  */
-  lcd.begin(16, 2);
-}
+
+
+
 void loop()
 {
   int tempReading = analogRead(tempPin);
@@ -27,16 +15,16 @@ void loop()
     float tempC = (tempVolts - 0.5) * 10.0;
     float tempF = tempC * 9.0 / 5.0 + 32.0;
   */
-  // Display Temperature in C
-  lcd.setCursor(0, 0);
-  lcd.print("Temp         C  ");
-  // Display Temperature in F
-  //lcd.print("Temp         F  ");
-  lcd.setCursor(6, 0);
-  // Display Temperature in C
-  lcd.print(tempC);
-  // Display Temperature in F
-  //lcd.print(tempF);
-  delay(500);
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
