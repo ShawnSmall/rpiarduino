ece2ea11864976359d245b028610d059789a6c9e
ShawnSmall <smsmall@umass.edu>
2020-10-26 23:38:50 +0000

    october 26

--- Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
+++ Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
@@ -1,21 +1,25 @@
+int tempReading=analogRead(tempPin);
+double tempK=log(10000.0*((1024.0/tempReading-1)));
+tempK=1/(0.001129148+(0.000234125+(0.0000000876741*tempK*tempK))*tempK);
+float tempC=tempK-273.15;
+float tempF=(tempC*9.0)/5.0+32.0
 
 
 
 
-void loop()
-{
-  int tempReading = analogRead(tempPin);
-  // This is OK
-  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
-  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
-  float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-  /*  replaced
-    float tempVolts = tempReading * 5.0 / 1024.0;
-    float tempC = (tempVolts - 0.5) * 10.0;
-    float tempF = tempC * 9.0 / 5.0 + 32.0;
-  */
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
