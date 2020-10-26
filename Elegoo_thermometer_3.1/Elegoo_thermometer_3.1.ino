869cc6de7d16c7fa5f1ea7360144bcc50ccb747c
ShawnSmall <smsmall@umass.edu>
2020-10-26 23:41:05 +0000

    typo corrected

--- Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
+++ Elegoo_thermometer_3.1/Elegoo_thermometer_3.1.ino
@@ -2,7 +2,7 @@ int tempReading=analogRead(tempPin);
 double tempK=log(10000.0*((1024.0/tempReading-1)));
 tempK=1/(0.001129148+(0.000234125+(0.0000000876741*tempK*tempK))*tempK);
 float tempC=tempK-273.15;
-float tempF=(tempC*9.0)/5.0+32.0
+float tempF=(tempC*9.0)/5.0+32.0;
 
 
 
